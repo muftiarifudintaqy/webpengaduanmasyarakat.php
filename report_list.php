121a3c918e28e57232b73e4c203a37df18c084d7
muftiarifudintaqy <muftiarifudintaqy0@gmail.com>
2024-11-15 14:14:54 +0700

    update di register.html

--- report_list.php
+++ report_list.php
@@ -23,14 +23,17 @@ $allUsers = array();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LAPOR! | Manager Dashboard</title>
     <style>
-      @import url('https://fonts.googleapis.com/css2?family=Oxanium:wght@200..800&family=Space+Grotesk:wght@300..700&display=swap');
-      * {
-        font-family: "Oxanium", sans-serif;
-      }
+        @import url('https://fonts.googleapis.com/css2?family=Oxanium:wght@200..800&family=Space+Grotesk:wght@300..700&display=swap');
+
+        * {
+            font-family: "Oxanium", sans-serif;
+        }
     </style>
     <link rel="shortcut icon" href="./assets/LAPOR_SMALL.png" type="image/x-icon">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css"></head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+</head>
 
 <body>
     <div class="container my-5">
@@ -38,7 +41,7 @@ $allUsers = array();
             <div class="card-header">
                 <h2>Manager Dashboard</h2>
                 <p>
-                  <?php 
+                    <?php
                     include './helper_php/db_init.php';
                     $sql = "SELECT * FROM users";
                     $result = $conn->query($sql);
@@ -47,7 +50,7 @@ $allUsers = array();
                         // Fetch all rows as an associative array
                         $allUsers = $result->fetch_all(MYSQLI_ASSOC);
                     }
-                  ?>
+                    ?>
                 </p>
             </div>
 
@@ -84,35 +87,35 @@ $allUsers = array();
 
                         <?php
 
-$id = $_SESSION["id"];
-$sql = ($_SESSION["role"]=="admin") ?
-    "SELECT reports.*, users.username FROM reports JOIN users ON users.id = reports.issuer_id;" :
-    "SELECT reports.*, users.username FROM reports JOIN users ON users.id = reports.issuer_id WHERE reports.issuer_id = $id;";
-$result = $conn->query($sql);
+                        $id = $_SESSION["id"];
+                        $sql = ($_SESSION["role"] == "admin") ?
+                            "SELECT reports.*, users.username FROM reports JOIN users ON users.id = reports.issuer_id;" :
+                            "SELECT reports.*, users.username FROM reports JOIN users ON users.id = reports.issuer_id WHERE reports.issuer_id = $id;";
+                        $result = $conn->query($sql);
 
-// function test_perms($var)
+                        // function test_perms($var)
 // {
 //   return($var["role"] != $_SESSION["role"]);
 // }
+                        
+                        while ($row = $result->fetch_assoc()) {
 
-while($row = $result->fetch_assoc()) {
+                            echo "<tr>";
+                            echo "<td>{$row["username"]}</td>";
+                            if (empty($row["subject"])): ?>
+                                <td>N/A</td>
+                            <?php else: ?>
+                                <td><?php echo $row["subject"]; ?></td>
+                            <?php endif;
+                            if (empty($row["message"])): ?>
+                                <td>N/A</td>
+                            <?php else: ?>
+                                <td><?php echo $row["message"]; ?></td>
+                            <?php endif;
+                            echo "<td>{$row["date_of_reporting"]}</td>";
 
-    echo "<tr>";
-    echo "<td>{$row["username"]}</td>";
-    if (empty($row["subject"])): ?>
-        <td>N/A</td>
-    <?php else: ?>
-        <td><?php echo $row["subject"]; ?></td>
-    <?php endif;
-    if (empty($row["message"])): ?>
-        <td>N/A</td>
-    <?php else: ?>
-        <td><?php echo $row["message"]; ?></td>
-    <?php endif;
-    echo "<td>{$row["date_of_reporting"]}</td>";
-
-}
-?>
+                        }
+                        ?>
 
                     </tbody>
                 </table>
